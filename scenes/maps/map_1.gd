0e3d29968f7da83aefde8fbda7823acc7fdbb29c
FlukyRookie <nicotresserrasmerino@gmail.com>
2024-06-24 23:58:54 +0200

    Paredes de mapa temp y sala #5
    Mapa 5 junto a sus enemigos, paredes y puertas (sin función aún).
    
    Paredes, y puertas (sin función excepto la de la derecha) de la sala temporal

--- scenes/maps/map_1.gd
+++ scenes/maps/map_1.gd
@@ -4,4 +4,4 @@ extends Node2D
 
 func _on_door_right_body_entered(body: PhysicsBody2D):
 	if body.is_in_group("Player"):
-		get_tree().call_deferred("change_scene_to_file", "res://scenes/maps/map_start.tscn")
+		get_tree().call_deferred("change_scene_to_file", "res://scenes/maps/map_5.tscn")
