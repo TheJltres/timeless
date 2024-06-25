1f2dd990e8264aaae0ea33f50017d7082bc7b257
FlukyRookie <nicotresserrasmerino@gmail.com>
2024-06-25 18:03:26 +0200

    Mapa 1, 2 y 3 añadidos

--- scenes/maps/map_0.gd
+++ scenes/maps/map_0.gd
@@ -3,10 +3,9 @@ extends Node2D
 @onready var player = $Player
 @onready var enemies = $Enemies
 
-
-func _on_door_up_body_entered(body):
+func _on_door_right_body_entered(body: PhysicsBody2D):
 	if enemies.get_child_count() > 0:
 		return
 
 	if body.is_in_group("Player"):
-		get_tree().call_deferred("change_scene_to_file", "res://scenes/maps/map_2.tscn")
+		get_tree().call_deferred("change_scene_to_file", "res://scenes/maps/map_1.tscn")
