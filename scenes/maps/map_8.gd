78cc95d62baec1044106846462e4e5f073255fc5
FlukyRookie <nicotresserrasmerino@gmail.com>
2024-06-29 18:35:50 +0200

    Tp de mapa 8 arreglado

--- scenes/maps/map_8.gd
+++ scenes/maps/map_8.gd
@@ -11,10 +11,11 @@ func _on_door_left_body_entered(body: PhysicsBody2D):
 	if body.is_in_group("Player"):
 		get_tree().call_deferred("change_scene_to_file", "res://scenes/maps/map_9.tscn")
 		
+		
 
 func _on_door_right_body_entered(body: PhysicsBody2D):
 	if enemies.get_child_count() > 0:
 		return
 
 	if body.is_in_group("Player"):
-		get_tree().call_deferred("change_scene_to_file", "res://scenes/maps/map_6.tscn")
+		get_tree().call_deferred("change_scene_to_file", "res://scenes/maps/map_5.tscn")
