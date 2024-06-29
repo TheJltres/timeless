60ade4c1ab75cf38828f95458c76a28a7a486ae9
FlukyRookie <nicotresserrasmerino@gmail.com>
2024-06-29 18:10:26 +0200

    Fondos de mapas y hitboxes arregladas
    Faltan mapas 8,9,10

--- scenes/maps/map_8.gd
+++ scenes/maps/map_8.gd
@@ -4,18 +4,17 @@ extends Node2D
 @onready var enemies = $Enemies
 
 
+func _on_door_left_body_entered(body: PhysicsBody2D):
+	if enemies.get_child_count() > 0:
+		return
+
+	if body.is_in_group("Player"):
+		get_tree().call_deferred("change_scene_to_file", "res://scenes/maps/map_9.tscn")
+		
 
 func _on_door_right_body_entered(body: PhysicsBody2D):
 	if enemies.get_child_count() > 0:
 		return
 
 	if body.is_in_group("Player"):
-		get_tree().call_deferred("change_scene_to_file", "res://scenes/maps/map_5.tscn")
-
-
-func _on_door_up_body_entered(body: PhysicsBody2D):
-	if enemies.get_child_count() > 0:
-		return
-
-	if body.is_in_group("Player"):
-		get_tree().call_deferred("change_scene_to_file", "res://scenes/maps/map_9.tscn")
+		get_tree().call_deferred("change_scene_to_file", "res://scenes/maps/map_6.tscn")
--- scenes/maps/map_9.gd
+++ scenes/maps/map_9.gd
@@ -4,17 +4,18 @@ extends Node2D
 @onready var enemies = $Enemies
 
 
+
+func _on_door_right_body_entered(body: PhysicsBody2D):
+	if enemies.get_child_count() > 0:
+		return
+
+	if body.is_in_group("Player"):
+		get_tree().call_deferred("change_scene_to_file", "res://scenes/maps/map_8.tscn")
+
+
 func _on_door_up_body_entered(body: PhysicsBody2D):
 	if enemies.get_child_count() > 0:
 		return
 
 	if body.is_in_group("Player"):
 		get_tree().call_deferred("change_scene_to_file", "res://scenes/maps/map_10.tscn")
-		
-
-func _on_door_down_body_entered(body: PhysicsBody2D):
-	if enemies.get_child_count() > 0:
-		return
-
-	if body.is_in_group("Player"):
-		get_tree().call_deferred("change_scene_to_file", "res://scenes/maps/map_8.tscn")
