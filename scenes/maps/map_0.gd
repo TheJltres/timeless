8058f872901e36c5f83d3c6084fc8d83911b907d
TheJltres <joseluistresserras@gmail.com>
2024-08-03 11:10:18 +0200

    Project update

--- scenes/maps/map_0.gd
+++ scenes/maps/map_0.gd
@@ -4,8 +4,8 @@ extends Node2D
 @onready var enemies = $Enemies
 
 func _on_door_right_body_entered(body: PhysicsBody2D):
-	if enemies.get_child_count() > 0:
-		return
+    if enemies.get_child_count() > 0:
+        return
 
-	if body.is_in_group("Player"):
-		get_tree().call_deferred("change_scene_to_file", "res://scenes/maps/map_1.tscn")
+    if body.is_in_group("Player"):
+        get_tree().call_deferred("change_scene_to_file", "res://scenes/maps/map_1.tscn")
